7e68ea79442d5bf3b856b9edadc2c9d7b334c012
Sergey Pushkovskiy <sergey@MacBook-Pro-User.local>
2020-09-21 00:27:37 +0300

    Fixed map and translations

--- src/components/utils/GoogleMap.vue
+++ src/components/utils/GoogleMap.vue
@@ -5,7 +5,7 @@
         width="100%"
         height="100%"
         id="gmap_canvas"
-        src="https://maps.google.com/maps?q=%D1%83%D0%BB%D0%B8%D1%86%D0%B0%20%D0%95%D0%B2%D0%B3%D0%B5%D0%BD%D0%B0%20%D0%A1%D0%B2%D0%B5%D1%80%D1%81%D1%82%D1%8E%D0%BA%D0%B0%2C%2011%D0%91&t=&z=15&ie=UTF8&iwloc=&output=embed"
+        src="https://maps.google.com/maps?q=%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F%20%D0%84%D0%B2%D0%B3%D0%B5%D0%BD%D0%B0%20%D0%A1%D0%B2%D0%B5%D1%80%D1%81%D1%82%D1%8E%D0%BA%D0%B0%2C%2011%D0%90%2C%20%D0%9A%D0%B8%D0%B5%D0%B2&t=&z=15&ie=UTF8&iwloc=&output=embed"
         frameborder="0"
         scrolling="no"
         marginheight="0"
